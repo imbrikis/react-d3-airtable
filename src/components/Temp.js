77459cf2fb8a52dd7f97cd434c2813ec18689b5f
imbrikis <imbrikis@yahoo.com>
2021-03-24 15:22:22 -0600

    first commit

---
+++ src/components/Temp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import d3 from 'd3'
+
+const Temp = () => {
+  return (
+    <div>
+      <h1>About to get D3-ed up in here</h1>
+    </div>
+  )
+}
+
+export default Temp
